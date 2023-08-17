f2619ef2bfdb54346bb9218d5f49b2e98a09e773
moondolph <jysim1000@gmail.com>
2023-08-18 00:16:06 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -9,3 +9,12 @@ document.addEventListener("scroll", () => {
     header.classList.remove("header--dark");
   }
 });
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home__container = document.querySelector(".home__container");
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  home__container.style.opacity = 1 - window.scrollY / homeHeight;
+});
