82e7c2cb73d46e94194b827e00f227e51b2111c7
moondolph <jysim1000@gmail.com>
2025-02-17 19:32:07 +0900

    change type speed and testimonial

--- src/type.js
+++ src/type.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
-new TypeIt(".home__title--strong",{
-  loop:true,
-  speed:50,
-})  //world-class Coder| 
-.move(-17)  //|world-class Coder
-.type("Amazing ") //Amazing |world-class Coder
-.move(null,{to:'END'})  //Amazing world-class Coder|
-.delete()  //|
-.type("creative front-end developer") //creative front-end developer|
-.move(-20)  //|creative front-end developer
-.delete()  //|
-.type("passionate") //passionate|
-.move(null,{to:'END'}) //passionate front-end developer| 
-.delete()  //|
-.go();
+new TypeIt(".home__title--strong", {
+  loop: true,
+  speed: 100,
+}) //world-class Coder|
+  .move(-17) //|world-class Coder
+  .type("Amazing ") //Amazing |world-class Coder
+  .move(null, { to: "END" }) //Amazing world-class Coder|
+  .delete() //|
+  .type("creative front-end developer") //creative front-end developer|
+  .move(-20) //|creative front-end developer
+  .delete() //|
+  .type("passionate") //passionate|
+  .move(null, { to: "END" }) //passionate front-end developer|
+  .delete() //|
+  .go();
